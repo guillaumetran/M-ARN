d0f0a7014748e9a1aaa6e7fd87466f0c5e92cf0d
heraud_b <baptiste.heraud@epitech.eu>
2020-02-15 10:47:22 +0100

    Secure

--- load.py
+++ load.py
@@ -8,11 +8,19 @@ def usage():
   print("Exemple : >rna AGCAGGGCAGAAGCCCACAUGGUU ....((((....))((....))))")
   sys.exit(0)
 
-def check(symbol, sequence):
+def checkSize(symbol, sequence):
     if len(symbol) != len(sequence):
-        print ("Symbol and sequence me be same size")
+        print ("[-] Symbol and sequence me be same size")
         sys.exit(0)
+    print ("[+] OK arguments size")
 
+def checkFormatTxt(txt):
+    bOpen = txt.count('(')
+    bClose = txt.count(')')
+    if bOpen != bClose:
+        print ("[-] Bad number of brackets")
+        sys.exit(0)
+    print ("[+] OK brackets")
 
 if __name__ == '__main__':
     if len(sys.argv) != 4 :
@@ -20,4 +28,5 @@ if __name__ == '__main__':
     nameMolecul = sys.argv[1]
     SymbolMolecul = sys.argv[2]
     TxtMolecul = sys.argv[3]
-    check(SymbolMolecul, TxtMolecul)
+    checkSize(SymbolMolecul, TxtMolecul)
+    checkFormatTxt(TxtMolecul)
